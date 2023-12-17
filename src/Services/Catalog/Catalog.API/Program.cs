5e83653d4125271886566f092cb2b50c53cb1eb4
annamone <annagventsadze9@gmail.com>
2023-12-17 23:51:32 +0400

    added controllers, repostiories, contexts, entities

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,5 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,6 +18,9 @@ namespace Catalog.API
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
+
+			builder.Services.AddScoped<ICatalogContext,CatalogContext>();
+			builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 			var app = builder.Build();
 
