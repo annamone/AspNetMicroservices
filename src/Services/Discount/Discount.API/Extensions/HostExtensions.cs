f728d7365ae785cdc11ef3c2db0f2132a49cfbda
annamone <annagventsadze9@gmail.com>
2023-12-30 19:02:30 +0400

    add controllers, repositories and docker compose

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+
+namespace Discount.API.Extensions
+{
+	public static class HostExtensions
+	{
+		public static IServiceProvider MigrateDatabase<TContext>(this IServiceProvider sp, int? retry = 0)
+		{
+			int retryForAvailability = retry.Value;
+
+			var configuration = sp.GetRequiredService<IConfiguration>();
+			var logger = sp.GetRequiredService<ILogger<TContext>>();
+
+			try
+			{
+				logger.LogInformation("Migrating postgresql database");
+
+				using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+				connection.Open();
+
+				using var command = new NpgsqlCommand
+				{
+					Connection = connection
+				};
+
+				command.CommandText = "DROP TABLE IF EXISTS Coupon";
+				command.ExecuteNonQuery();
+
+				command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+														    ProductName VARCHAR(24) NOT NULL,
+                                                            Description TEXT,
+															Amount INT)";
+				command.ExecuteNonQuery();
+
+				command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+				command.ExecuteNonQuery();
+
+				command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+				command.ExecuteNonQuery();
+
+				logger.LogInformation("Migrated postresql database.");
+			}
+			catch (NpgsqlException ex)
+			{
+				logger.LogError(ex, "An error occurred while migrating the postresql database");
+
+				if (retryForAvailability < 50)
+				{
+					retryForAvailability++;
+					System.Threading.Thread.Sleep(2000);
+					MigrateDatabase<TContext>(sp, retryForAvailability);
+				}
+			}
+
+			return sp;
+		}
+	}
+}
--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,9 @@
+using Discount.API.Extensions;
+using Discount.API.Respositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace Discount.API
 {
 	public class Program
@@ -7,6 +13,7 @@ namespace Discount.API
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
+			builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,6 +21,7 @@ namespace Discount.API
 			builder.Services.AddSwaggerGen();
 
 			var app = builder.Build();
+			app.Services.MigrateDatabase<Program>();
 
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
