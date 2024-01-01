8dd4741e2bbb1dcabaecb96a6e87ae964f011eaa
annamone <annagventsadze9@gmail.com>
2024-01-01 22:18:41 +0400

    added class libreries into Ordering folder and updated Order.Application

---
+++ src/Services/Ordering/Ordering.Application/Features/Queries/GetOrderList/GetOrdersListQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Features.Queries.GetOrderList
+{
+	public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+	{
+		public string UserName { get; set; }
+
+		public GetOrdersListQuery(string userName)
+		{
+			UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+		}
+	}
+}
