8b7f27309eacb33caac659a269e164b8f44ea043
annamone <annagventsadze9@gmail.com>
2024-01-03 20:08:46 +0400

    added rabbitmq consumer in ordering.api

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,10 @@
+using EventBus.Messages.Common;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.API.EventBusConsumer;
 using Ordering.API.Extensions;
 using Ordering.Application;
 using Ordering.Infrastructure;
@@ -19,6 +22,24 @@ namespace Ordering.API
 
 			builder.Services.AddApplicationServices();
 			builder.Services.AddInfrastructureServices(builder.Configuration);
+
+			// MassTransit-RabbitMQ Configuration
+			builder.Services.AddMassTransit(config =>
+			{
+				config.AddConsumer<BasketCheckoutConsumer>();
+				config.UsingRabbitMq((ctx, cfg) =>
+				{
+					cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+					cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+					{
+						c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+					});
+				});
+			});
+
+			// General Configuration
+			builder.Services.AddAutoMapper(typeof(Program));
+			builder.Services.AddScoped<BasketCheckoutConsumer>();
 
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
