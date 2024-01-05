193b943b9ea155b75892afda46d1deddbb586191
annamone <annagventsadze9@gmail.com>
2024-01-05 19:10:25 +0400

    added api gateway

---
+++ src/ApiGateWays/OcelotApiGw/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Threading.Tasks;
+
+namespace OcelotApiGw
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+			{
+				config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+			});
+			builder.Services.AddOcelot(builder.Configuration);
+
+			builder.Services.AddLogging(loggingBuilder =>
+			{
+				loggingBuilder.AddConfiguration(builder.Configuration.GetSection("Logging"));
+				loggingBuilder.AddConsole();
+				loggingBuilder.AddDebug();
+			});
+
+			var app = builder.Build();
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapGet("/", async context =>
+				{
+					await context.Response.WriteAsync("Hello World!");
+				});
+			});
+
+			await app.UseOcelot();
+
+			app.Run();
+		}
+	}
+}
--- src/Services/Discount/Discount.API/Controllers/DiscountController.cs
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Discount.API.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("api/v1/[controller]")]
 	[ApiController]
 	public class DiscountController : ControllerBase
 	{
